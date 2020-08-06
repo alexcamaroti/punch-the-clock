18ccbc305a5af0db8a064cde18f846166829f2bc
Alex Camaroti <acamaroti@gmail.com>
2020-08-06 15:55:33 -0300

    Base Project

---
+++ src/main/java/br/com/camaroti/alex/punchtheclock/PunchtheclockApplication.java
@@ -0,0 +1,13 @@
+package br.com.camaroti.alex.punchtheclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchtheclockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchtheclockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/camaroti/alex/punchtheclock/PunchtheclockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.camaroti.alex.punchtheclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchtheclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
